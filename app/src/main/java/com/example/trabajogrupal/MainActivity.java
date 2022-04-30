4679123257ea1c2f90d25d1f018df223628cb941
UnaiH <71124187+UnaiH@users.noreply.github.com>
2022-04-30 22:22:07 +0200

    SignIn Layout

--- app/src/main/java/com/example/trabajogrupal/MainActivity.java
+++ app/src/main/java/com/example/trabajogrupal/MainActivity.java
@@ -2,13 +2,68 @@ package com.example.trabajogrupal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import androidx.work.Data;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
+import androidx.lifecycle.Observer;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+    public void onClickSignIn(View view) {
+        EditText user=findViewById(R.id.userSignIn);
+        EditText pass=findViewById(R.id.PasswordSignIn);
+        Data.Builder data = new Data.Builder();
+        data.putString("usuario",user.getText().toString());
+        data.putString("contrasena",pass.getText().toString());
+        OneTimeWorkRequest otwr = new OneTimeWorkRequest.Builder(PHPSignIn.class)
+                .setInputData(data.build())
+                .build();
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(otwr.getId())
+                .observe(this, new Observer<WorkInfo>() {
+                    @Override
+                    public void onChanged(WorkInfo workInfo)
+                    {
+                        //Si se puede iniciar sesión porque devulve true se cambiará la actividad cerrando en la que se encuentra. Si la devolución es null o no es true se mostrará un toast en la interfaz actual.
+                        if(workInfo != null && workInfo.getState().isFinished())
+                        {
+                            String inicio = workInfo.getOutputData().getString("result");
+                            Log.i("TAG", "onChanged: "+inicio);
+                            if (inicio!=null) {
+                                if (inicio.equals("true")) {
+                                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                                    i.putExtra("usuario", user.getText().toString());
+                                    setResult(RESULT_OK, i);
+                                    finish();
+                                    startActivity(i);
+                                } else {
+                                }
+                            }
+                            else {
+                            }
+                        }
+                    }
+                });
+        WorkManager.getInstance(this).enqueue(otwr);
+    }
+    public void onClickSignUp(View view) {
+        finish();
+        Intent i = new Intent(this, SignUp.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/trabajogrupal/PHPSignIn.java
@@ -0,0 +1,84 @@
+package com.example.trabajogrupal;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.Data;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class PHPSignIn extends Worker{
+    public PHPSignIn(@NonNull Context context, @NonNull WorkerParameters workerParams)
+    {
+        super(context, workerParams);
+    }
+    @NonNull
+    @Override
+    public Result doWork()
+    {
+        String user = getInputData().getString("user");
+        String pass = getInputData().getString("pass");
+        Log.i("TAG1", "doWork: "+user);
+        Log.i("TAG1", "doWork: "+pass);
+        String direccion = "";
+        HttpURLConnection urlConnection = null;
+        try
+        {
+            URL destino = new URL(direccion);
+            urlConnection = (HttpURLConnection) destino.openConnection();
+            urlConnection.setConnectTimeout(5000);
+            urlConnection.setReadTimeout(5000);
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setDoOutput(true);
+            urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            String parametros = "usu="+user+"&contrasena="+pass;
+            PrintWriter out = new PrintWriter(urlConnection.getOutputStream());
+            out.print(parametros);
+            out.close();
+            Log.i("TAG","statusCode: " + urlConnection);
+            int statusCode = urlConnection.getResponseCode();
+            Log.i("TAG","statusCode: " + statusCode);
+            if (statusCode == 200)
+            {
+                BufferedInputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                String line, result = "";
+                while ((line = bufferedReader.readLine()) != null) {
+                    result += line;
+                }
+                inputStream.close();
+                JSONArray jsonArray = new JSONArray(result);
+                String resultado="";
+                for(int i = 0; i < jsonArray.length(); i++)
+                {
+                    Log.i("TAG", "doWork: "+jsonArray.getJSONObject(i));
+                    resultado = jsonArray.getJSONObject(i).getString("resultado");
+                }
+                Data json = new Data.Builder()
+                        .putString("result",resultado)
+                        .build();
+                Log.i("php","listaJson: " + json);
+                return Result.success(json);
+            }
+            return Result.failure();
+        }
+        catch (MalformedURLException e) {e.printStackTrace();}
+        catch (IOException e) {e.printStackTrace();}
+        catch (JSONException e) {e.printStackTrace();}
+        return Result.failure();
+    }
+}
