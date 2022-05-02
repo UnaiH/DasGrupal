263887e8a3a792a6d6693c7cd9171a0160077818
UnaiH <71124187+UnaiH@users.noreply.github.com>
2022-05-02 12:00:48 +0200

    some changes

--- app/src/main/java/com/example/trabajogrupal/globalRankingActivity.java
+++ app/src/main/java/com/example/trabajogrupal/globalRankingActivity.java
@@ -6,11 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 
 public class globalRankingActivity extends AppCompatActivity {
-
+    private String[] usernames;
+    private int[] elos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_global_ranking);
+        globalRankingAdapter adaptador = new globalRankingAdapter(this, usernames, elos);
+        //Se llama al adaptador de esta lista.
     }
     public void onBackPressed() {
         finish();
--- app/src/main/java/com/example/trabajogrupal/globalRankingAdapter.java
+++ app/src/main/java/com/example/trabajogrupal/globalRankingAdapter.java
@@ -1,23 +1,34 @@
 package com.example.trabajogrupal;
 
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
 public class globalRankingAdapter extends BaseAdapter {
+    private LayoutInflater inflater;
+    private String[] userNames;
+    private int[] ELO;
+    public globalRankingAdapter(Activity context, String[] pUserNames,int[] pELO){
+        userNames=pUserNames;
+        ELO=pELO;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
     @Override
     public int getCount() {
-        return 0;
+        return userNames.length;
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return userNames[i];
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
