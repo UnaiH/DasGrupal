a41a978d4580e916202b54b911b9531c81ed4ca9
PabloRehecho <71124141+PabloRehecho@users.noreply.github.com>
2022-05-17 20:19:58 +0200

    Add files via upload

---
+++ PHP/UpdatePiece.php
@@ -0,0 +1,32 @@
+<?php
+
+$DB_SERVER="127.0.0.1"; #la dirección del servidor
+$DB_USER="Xprehecho001"; #el usuario para esa base de datos
+$DB_PASS="*NnqYQE8a10"; #la clave para ese usuario
+$DB_DATABASE="Xprehecho001_Chess"; #la base de datos a la que hay que conectarse
+# Se establece la conexión:
+$con = mysqli_connect($DB_SERVER, $DB_USER, $DB_PASS, $DB_DATABASE);
+#Comprobamos conexión
+if (mysqli_connect_errno($con)) 
+{
+	echo 'Error de conexion: ' . mysqli_connect_error();
+	exit();
+}
+
+$parametro = $_POST["idGame"];
+$parametro2 = $_POST["posXOld"];
+$parametro3 = $_POST["posYOld"];
+$parametro4 = $_POST["posXNew"];
+$parametro5 = $_POST["posYNew"];
+
+
+# Ejecutar la sentencia SQL
+$resultado = mysqli_query($con, "UPDATE Piece SET posX=$parametro4, posY=$parametro5 
+WHERE idGame=$parametro AND posX=$parametro2 AND posY=$parametro3");
+
+# Comprobar si se ha ejecutado correctamente
+if (!$resultado) 
+{
+	echo 'Ha ocurrido algún error: ' . mysqli_error($con);
+}
+?>
