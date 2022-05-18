74720a01c56c967d2da696a259c7fa35873461d3
DannyelYankee <73075185+DannyelYankee@users.noreply.github.com>
2022-05-18 19:32:27 +0200

    php para el ranking

---
+++ PHP/getUsersByCountryChess.php
@@ -0,0 +1,34 @@
+<?php
+$DB_SERVER="localhost";
+$DB_USER="Xprehecho001";
+$DB_PASS="*NnqYQE8a10"; 
+$DB_DATABASE="Xprehecho001_Chess";
+
+$con = mysqli_connect($DB_SERVER, $DB_USER, $DB_PASS, $DB_DATABASE);
+
+if (mysqli_connect_errno($con)){
+        echo 'Error de conexion: ' . mysqli_connect_error();
+        exit();
+}
+
+
+$email = $_POST["email"];
+$country =$_POST["country"];
+
+$stmt = mysqli_query($con,"SELECT * FROM Usuario GROUP BY Usuario.country ORDER BY Usuario.eloChess Desc");
+if ($stmt->num_rows > 0){
+	while($row = $stmt->fetch_assoc()){
+		$arrayresultado =array(
+		'email'=>$row['email'],
+		'username'=>$row['username'],		
+		'eloChess'=>$row['eloChess'],
+		'country'=>$row['country']);	
+		$resultado[] = array('resultado' => $arrayresultado);
+	}	
+}else{
+	$resultado[] = array('resultado' => false);
+}
+echo json_encode($resultado);
+mysqli_close($con)
+
+?>
---
+++ PHP/insertCountry.php
@@ -0,0 +1,29 @@
+<?php
+$DB_SERVER="localhost";
+$DB_USER="Xprehecho001";
+$DB_PASS="*NnqYQE8a10"; 
+$DB_DATABASE="Xprehecho001_Chess";
+
+$con = mysqli_connect($DB_SERVER, $DB_USER, $DB_PASS, $DB_DATABASE);
+
+if (mysqli_connect_errno($con)){
+        echo 'Error de conexion: ' . mysqli_connect_error();
+        exit();
+}
+
+
+$email = $_POST["email"];
+$country =$_POST["country"];
+$stmt=mysqli_query($con,"SELECT * FROM Usuario WHERE email='$email'");
+if(mysqli_num_rows($stmt)==0){
+    mysqli_query($con,"INSERT INTO Usuario(country) VALUES ('$country') WHERE email='$email'");
+    $resultado[] = array('resultado' => true);
+}else{
+    $resultado[] = array('resultado' => false);
+}
+
+
+echo json_encode($resultado);
+mysqli_close($con)
+
+?>
